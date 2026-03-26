5da8a84b2f436d5b5e52913ccf2f631f404ce329
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-26 19:50:14 +0900

    STEP1 리스트 생성 및 Product 생성자 설정

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,14 @@
 public class Product {
+    private String name;
+    private int price;
+    private String explain;
+    private int stock;
+
+    public Product(String name, int price, String explain)
+    {
+        this.name = name;
+        this.price = price;
+        this.explain = explain;
+    }
 
 }
