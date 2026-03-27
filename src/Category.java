3c639c5693a8aa39e8b5fb35996d99847faf768b
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-27 11:35:11 +0900

    카테고리 이름 배열로 변경

--- src/Category.java
+++ src/Category.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
-    private String name;
-    List<Product> products;
-    List<Product> electroProducts = new ArrayList<>();
-    List<Product> clothesProducts = new ArrayList<>();
-    List<Product> foodProducts = new ArrayList<>();
+    private String[] name = {"전자제품", "의류", "식품"};
+    private List<Product> products;
+    private List<Product> electroProducts = new ArrayList<>();
+    private List<Product> clothesProducts = new ArrayList<>();
+    private List<Product> foodProducts = new ArrayList<>();
 
 
     public Category(List<Product> product)
