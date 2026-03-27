16ebb6ce6d86ff16260d705217397c926f682018
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-27 11:29:32 +0900

    STEP3 시작 커밋

--- src/Category.java
+++ src/Category.java
@@ -4,6 +4,10 @@ import java.util.List;
 public class Category {
     private String name;
     List<Product> products;
+    List<Product> electroProducts = new ArrayList<>();
+    List<Product> clothesProducts = new ArrayList<>();
+    List<Product> foodProducts = new ArrayList<>();
+
 
     public Category(List<Product> product)
     {
